2f7cb400bdad2e4d9e494e2784fc0d755260e512
jowsingchue <https://jowsingchue@github.com>
2014-11-14 16:06:39 +0700

    testing with excessive memory

--- sproject.ino
+++ sproject.ino
@@ -27,11 +27,14 @@ Wiring:
 */
 
 
-// MPU-6050
+// MPU-6050//
 #include "I2Cdev.h"
-#include "MPU6050.h"
+// #include "MPU6050.h"
+#include "MPU6050_6Axis_MotionApps20.h"
+
 // SD card
 #include <SD.h>
+
 // GPS
 #include <SoftwareSerial.h>
 #include <TinyGPS.h>
@@ -51,16 +54,47 @@ MPU6050 accelgyro;
 int16_t ax, ay, az;
 int16_t gx, gy, gz;
 
+MPU6050 mpu;
+
 // uncomment "OUTPUT_READABLE_ACCELGYRO" if you want to see a tab-separated
 // list of the accel X/Y/Z and then gyro X/Y/Z values in decimal. Easy to read,
 // not so easy to parse, and slow(er) over UART.
-#define OUTPUT_READABLE_ACCELGYRO
+// #define OUTPUT_READABLE_ACCELGYRO
 
 // uncomment "OUTPUT_BINARY_ACCELGYRO" to send all 6 axes of data as 16-bit
 // binary, one right after the other. This is very fast (as fast as possible
 // without compression or data loss), and easy to parse, but impossible to read
 // for a human.
 // #define OUTPUT_BINARY_ACCELGYRO
+
+
+
+#define OUTPUT_READABLE_REALACCEL
+// MPU control/status vars
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+
+// orientation/motion vars
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorInt16 aa;         // [x, y, z]            accel sensor measurements
+VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
+VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
+VectorFloat gravity;    // [x, y, z]            gravity vector
+float euler[3];         // [psi, theta, phi]    Euler angle container
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+
+// ================================================================
+// ===               INTERRUPT DETECTION ROUTINE                ===
+// ================================================================
+
+volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
+void dmpDataReady() {
+    mpuInterrupt = true;
+}
 /* --- End MPU-6050 Config --- */
 
 
@@ -110,6 +144,37 @@ void setup()
     Serial.println("Testing device connections...");
     Serial.println(accelgyro.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
 
+
+    // load and configure the DMP
+    Serial.println(F("Initializing DMP..."));
+    devStatus = mpu.dmpInitialize();
+    
+    // make sure it worked (returns 0 if so)
+    if (devStatus == 0) {
+        // turn on the DMP, now that it's ready
+        Serial.println(F("Enabling DMP..."));
+        mpu.setDMPEnabled(true);
+
+        // enable Arduino interrupt detection
+        Serial.println(F("Enabling interrupt detection (Arduino external interrupt 0)..."));
+        attachInterrupt(0, dmpDataReady, RISING);
+        mpuIntStatus = mpu.getIntStatus();
+
+        // set our DMP Ready flag so the main loop() function knows it's okay to use it
+        Serial.println(F("DMP ready! Waiting for first interrupt..."));
+        dmpReady = true;
+
+        // get expected DMP packet size for later comparison
+        packetSize = mpu.dmpGetFIFOPacketSize();
+    } else {
+        // ERROR!
+        // 1 = initial memory load failed
+        // 2 = DMP configuration updates failed
+        // (if it's going to break, usually the code will be 1)
+        Serial.print(F("DMP Initialization failed (code "));
+        Serial.print(devStatus);
+        Serial.println(F(")"));
+    }
     // use the code below to change accel/gyro offset values
     /* 
     Serial.println("Updating internal sensor offsets...");
@@ -163,8 +228,54 @@ void setup()
 
 void loop()
 {   
+    // if programming failed, don't try to do anything
+    if (!dmpReady) return;
+
+    // wait for MPU interrupt or extra packet(s) available
+    while (!mpuInterrupt && fifoCount < packetSize) {
+        // other program behavior stuff here
+        // .
+        // .
+        // .
+        // if you are really paranoid you can frequently test in between other
+        // stuff to see if mpuInterrupt is true, and if so, "break;" from the
+        // while() loop to immediately process the MPU data
+        // .
+        // .
+        // .
+    }
+    // reset interrupt flag and get INT_STATUS byte
+    mpuInterrupt = false;
+    mpuIntStatus = mpu.getIntStatus();
+
+    // get current FIFO count
+    fifoCount = mpu.getFIFOCount();
+
+    // check for overflow (this should never happen unless our code is too inefficient)
+    if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
+        // reset so we can continue cleanly
+        mpu.resetFIFO();
+        Serial.println(F("FIFO overflow!"));
+
+    // otherwise, check for DMP data ready interrupt (this should happen frequently)
+    } else if (mpuIntStatus & 0x02) {
+        // wait for correct available data length, should be a VERY short wait
+        while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+
+        // read a packet from FIFO
+        mpu.getFIFOBytes(fifoBuffer, packetSize);
+        
+        // track FIFO count here in case there is > 1 packet available
+        // (this lets us immediately read more without waiting for an interrupt)
+        fifoCount -= packetSize;
+    }
+
     // read imu
-    accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+    // accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+    mpu.dmpGetQuaternion(&q, fifoBuffer);
+    mpu.dmpGetAccel(&aa, fifoBuffer);
+    mpu.dmpGetGravity(&gravity, &q);
+    mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
 
     // read gps
     float flat, flon;
@@ -173,9 +284,9 @@ void loop()
 
     // put data into a string
     String dataString = "";
-    dataString += String(ax) + ",";
-    dataString += String(ay) + ",";
-    dataString += String(az) + ",";
+    dataString += String(aaReal.x) + ",";
+    dataString += String(aaReal.y) + ",";
+    dataString += String(aaReal.z) + ",";
     dataString += String(gx) + ",";
     dataString += String(gy) + ",";
     dataString += String(gz) + ",";
@@ -211,7 +322,7 @@ static void print_imu_to_serial()
 {
     // MPU-6050
     #ifdef OUTPUT_READABLE_ACCELGYRO
-        // display tab-separated accel/gyro x/y/z values
+    // display tab-separated accel/gyro x/y/z values
     Serial.print("a/g:\t");
     Serial.print(ax); Serial.print("\t");
     Serial.print(ay); Serial.print("\t");
@@ -228,6 +339,15 @@ static void print_imu_to_serial()
     Serial.write((uint8_t)(gx >> 8)); Serial.write((uint8_t)(gx & 0xFF));
     Serial.write((uint8_t)(gy >> 8)); Serial.write((uint8_t)(gy & 0xFF));
     Serial.write((uint8_t)(gz >> 8)); Serial.write((uint8_t)(gz & 0xFF));
+    #endif
+
+    #ifdef OUTPUT_READABLE_REALACCEL
+    Serial.print("areal\t");
+    Serial.print(aaReal.x);
+    Serial.print("\t");
+    Serial.print(aaReal.y);
+    Serial.print("\t");
+    Serial.println(aaReal.z);
     #endif
 }
 
@@ -322,46 +442,3 @@ static void print_float(float val, float invalid, int len, int prec)
     smartdelay(0);
 }
 
-static void print_int(unsigned long val, unsigned long invalid, int len)
-{
-    char sz[32];
-    if (val == invalid)
-        strcpy(sz, "*******");
-    else
-        sprintf(sz, "%ld", val);
-
-    sz[len] = 0;
-    for (int i=strlen(sz); i<len; ++i)
-        sz[i] = ' ';
-    if (len > 0) 
-        sz[len-1] = ' ';
-    Serial.print(sz);
-    smartdelay(0);
-}
-
-static void print_date(TinyGPS &gps)
-{
-    int year;
-    byte month, day, hour, minute, second, hundredths;
-    unsigned long age;
-    gps.crack_datetime(&year, &month, &day, &hour, &minute, &second, &hundredths, &age);
-    if (age == TinyGPS::GPS_INVALID_AGE)
-        Serial.print("********** ******** ");
-    else
-    {
-        char sz[32];
-        sprintf(sz, "%02d/%02d/%02d %02d:%02d:%02d ",
-        month, day, year, hour, minute, second);
-        Serial.print(sz);
-    }
-    print_int(age, TinyGPS::GPS_INVALID_AGE, 5);
-    smartdelay(0);
-}
-
-static void print_str(const char *str, int len)
-{
-    int slen = strlen(str);
-    for (int i=0; i<len; ++i)
-        Serial.print(i<slen ? str[i] : ' ');
-    smartdelay(0);
-}
