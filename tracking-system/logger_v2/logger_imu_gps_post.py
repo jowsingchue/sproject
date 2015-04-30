5c293fee134ac96056c850e412f24098814bd89a
wootsaejao <wootsaejao@gmail.com>
2015-04-30 20:22:42 +0700

    update stuff

--- tracking-system/logger_v2/logger_imu_gps_post.py
+++ tracking-system/logger_v2/logger_imu_gps_post.py
@@ -19,11 +19,11 @@ import smbus
 device_id = 1234
 
 #	amr server
-#url = 'http://183.90.171.55:8080/log'
+url = 'http://183.90.171.55:8080/log'
 
 #	local
 #url = 'http://localhost:8080/log'
-url = 'http://192.168.1.131:8080/log'
+#url = 'http://192.168.1.131:8080/log'
 #url = 'http://192.168.43.155:8080/log'
 
 #imu_offset = [ 56, -142, -294, -246, 192, 144 ]
